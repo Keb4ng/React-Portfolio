bcdbe04102e61589d20cdeabee7df7cae00a57fc
Jon Trinidad <jontrinidad18@gmail.com>
2024-03-19 10:00:57 +0800

    Updated list

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ import fodpanda from "./Media/fodpanda.png";
 import localhost from "./Media/localhost.png";
 import schubert from "./Media/schubert.png";
 import portfolio1 from "./Media/porfolio-1.png";
+import gaganda from "./Media/gaganda.png";
 
 export const data = [
   {
@@ -9,24 +10,32 @@ export const data = [
     name: "Fodpanda",
     link: "https://fodpanda.vercel.app/",
     img: fodpanda,
-    desc: "A food ordering website with login and cart ordering system",
+    desc: "A food ordering website with login and cart ordering system. Built with React/Tailwind",
   },
   {
     id: 2,
+    name: "Gaganda",
+    link: "https://gaganda.vercel.app/",
+    img: gaganda,
+    desc: "A Website pulling from a Make-up Api with cart and Login system. Built with React/Tailwind",
+  },
+
+  {
+    id: 3,
     name: "Localhost",
     link: "https://react-projects-roan-five.vercel.app/",
     img: localhost,
-    desc: "A booking website mock-up used to practice react/tailwind. built with tailwind and react",
+    desc: "A booking website mock-up used to practice react/tailwind. Built with React/Tailwind",
   },
   {
-    id: 3,
+    id: 4,
     name: "Schubert Real Estate",
     link: "https://keb4ng.github.io/realestate/",
     img: schubert,
     desc: "Real estate website mock-up conversion that is built using html/css/js",
   },
   {
-    id: 4,
+    id: 5,
     name: "Old Portfolio",
     link: "https://keb4ng.github.io/Demo/",
     img: portfolio1,
